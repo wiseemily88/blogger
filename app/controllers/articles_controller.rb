1cd75de7192665dff7a6a7058fb2290031ed7df6
Emily Wise <wise.m.emily@gmail.com>
2017-10-15 14:32:35 -0600

    Add Delete and Update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:destroy, :edit, :update]
 
   def index
     @articles = Article.all
@@ -15,13 +16,35 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+
+    flash.notice = "Article '#{@article.title}' was added!"
     redirect_to article_path(@article)
   end
 
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+   redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article.destroy
+
+    redirect_to articles_path
+  end
 
 private
 
   def article_params
     params.require(:article).permit(:title, :body)
   end
+
+  def set_article
+     @article = Article.find(params[:id])
+   end
 end
